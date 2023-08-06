b1ab7ebcdbc1077dd9f45da4b488215c6aa7d47c
Widdy LOUIS <louisws@outlook.fr>
2023-08-06 14:57:18 +0200

    Reusable Modal part1

---
+++ components/BaseModal.vue
@@ -0,0 +1,24 @@
+<script setup>
+  defineEmits(['close-modal'])
+  defineProps({
+    modalActive: {
+      type: Boolean,
+      default: false
+    }
+  });
+</script>
+<template>
+ <div 
+  v-show="modalActive"
+  class="flex justify-center absolute w-full bg-black bg-opacity-30 h-screen top-0 left-0 px-8">
+  <div 
+    v-if="modalActive"
+    class="p-4 bg-white self-start mt-32 max-w-screen-md">
+    <slot/>
+    <button 
+      @click="$emit('close-modal')"
+      class="text-white mt-8 bg-weather-primary py-2 px-6" >Close</button>
+  </div>
+ </div>
+</template>
+
