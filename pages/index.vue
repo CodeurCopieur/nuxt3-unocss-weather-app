8f488c33397c3a3087fd06910a6db405c709cde7
Widdy LOUIS <louisws@outlook.fr>
2023-08-08 23:45:26 +0200

    City Search part1

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,42 @@
+<script setup>
+
+  const state = reactive({
+    searchQuery: '',
+    queryTimeout: null,
+    searchResult: null,
+    allItems: []
+  })
+
+  async function handleBlur(event) {
+    event.preventDefault();
+    
+    state.allItems = await useWeatherApi().getSearchResults(state.queryTimeout, state.searchQuery)
+
+    state.searchQuery = ''
+
+    if (!state.searchQuery) {
+      state.allItems = []
+      return
+    }
+  }
+
+</script>
 <template>
- <h1 class="text-very-cool">Home</h1>
- <Icon 
-  name="ic:baseline-access-alarms" 
-  width="96"
-  :inline="true"
-  color="black" />
+  <main class="container mx-auto text-white px-4">
+    <form class="pt-4 mb-8 relative">
+      <input 
+        type="text" 
+        v-model="state.searchQuery"
+        @keypress.enter="handleBlur"
+        placeholder="Rechercher une ville ou un état"
+        class="py-4 px-1 w-full bg-transparent border-b focus:border-weather-secondary focus:outline-none focus:shadow-[0px_1px_0_0_#004E71]">
+    </form>
+  </main>
 </template>
 
-<script setup>
-  
-</script>
+<style scoped>
+input::placeholder {
+  color: #ccc;
+}
 
-<style lang="scss" scoped>
-  
 </style>
