26c33f1fc4ff8291f94513bd618f01cee2eab3b4
Widdy LOUIS <louisws@outlook.fr>
2023-08-09 18:23:45 +0200

    City Search part2

--- pages/index.vue
+++ pages/index.vue
@@ -13,10 +13,14 @@
     try{
       state.allItems = await useWeatherApi().getSearchResults(state.searchQuery)
 
-      const { data } = state.allItems;
-      const res = data.features;
+      setTimeout(()=> {
+        const { data } = state.allItems;
+        const res = data.features;
+        state.searchResult = res
+        console.log('Résultats de la recherche :', state.searchResult);
+      }, 300)
 
-      console.log('Résultats de la recherche :', res);
+      
     } catch (error) {
       console.error('Erreur lors de la recherche :', error);
     }
@@ -47,6 +51,15 @@
         @keypress.enter="handleBlur"
         placeholder="Rechercher une ville ou un état"
         class="py-4 px-1 w-full bg-transparent border-b focus:border-weather-secondary focus:outline-none focus:shadow-[0px_1px_0_0_#004E71]">
+
+        <ul 
+          v-if="state.searchResult"
+          class="absolute bg-weather-secondary text-white w-full shadow-md py-2 px-1 top-[66px]">
+          <li 
+            v-for="searchResult in state.searchResult" 
+            :key="searchResult.id"
+            class="py-2 cursor-pointer"> {{ searchResult.place_name }}</li>
+        </ul>
     </form>
 
     <div v-if="state.visible" class="container max-w-xs mx-auto bg-red-100 border border-red-400 text-red-700 px-4 py-3 relative mt-4 text-center" role="alert">
