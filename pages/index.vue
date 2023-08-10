e8c9377ccc055b9712714593a0c3c8e9f8cc2108
Widdy LOUIS <louisws@outlook.fr>
2023-08-10 22:58:00 +0200

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -10,34 +10,29 @@
 
   async function handleBlur(event) {
     event.preventDefault();
-    
 
     if (state.searchQuery === '') {
         state.allItems = []
+        state.searchResult = []
         state.visible = true
         state.messageError = "Désolé, quelque chose s'est mal passé, veuillez réessayer"
 
         return
-      } else if(state.allItems !== []) {
+      }  
+      
+      if(state.searchQuery !== '') {
         state.allItems = await useWeatherApi().getSearchResults(state.searchQuery)
+        const { data } = state.allItems;
+        const res = data.features;
+        state.searchResult = res
+        state.messageError = ''
 
-        // setTimeout(()=> {
-          const { data } = state.allItems;
-          const res = data.features;
-          state.searchResult = res
-        // }, 300)
-
-        state.visible = false
+        if (state.allItems = []) {
+          state.visible = false
+          state.messageError = "Aucun résultat ne correspond à votre requête, essayez un  terme différent."
+        }
+        
       }
-
-      if (state.allItems = []) {
-        state.visible = false
-        console.log(state.allItems = []);
-        state.messageError = "Aucun résultat ne correspond à votre requête, essayez un terme différent."
-      }
-
-
-    // state.searchQuery = ''
 
   }
 
@@ -53,8 +48,8 @@
         class="py-4 px-1 w-full bg-transparent border-b focus:border-weather-secondary focus:outline-none focus:shadow-[0px_1px_0_0_#004E71]">
 
         <ul 
-          v-if="state.searchResult"
-          class="absolute bg-weather-secondary text-white w-full shadow-md py-2 px-1 top-[66px]">
+          v-if="state.searchResult !== []"
+          class="absolute bg-weather-secondary text-white w-full shadow-md py-2 px-1 top-[66px] z-50">
           <li 
             v-for="searchResult in state.searchResult" 
             :key="searchResult.id"
@@ -62,7 +57,7 @@
         </ul>
     </form>
 
-    <div v-if="state.visible  || state.visible === false && state.allItems.length ===0" class="container mx-auto bg-red-100 border border-weather-tertiary text-weather-tertiary px-4 py-3 relative mt-4 text-center" role="alert">
+    <div v-if="state.visible === true  || state.visible === false && state.allItems.length === 0" class="container mx-auto bg-red-100 border border-weather-tertiary text-weather-tertiary px-4 py-3 relative mt-4 text-center" role="alert">
       <p>{{ state.messageError }}</p>
     </div>
   </main>
