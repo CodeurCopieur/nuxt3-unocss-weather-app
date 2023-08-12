2895644d8b26a5316f0d72bc0c13c2327d756b67
Widdy LOUIS <louisws@outlook.fr>
2023-08-12 22:49:10 +0200

    City View Layout, conversion Celsius part1

--- components/AsyncCityView.vue
+++ components/AsyncCityView.vue
@@ -2,6 +2,14 @@
   const route = useRoute();
   const {name} = route.params;
   const {lat, lng} = route.query;
+  const state = reactive({
+    currentTime: null,
+    temp: null
+  });
+
+  const convertToCelsius  = (fahrenheit) => {
+    return (fahrenheit - 32) * 5 / 9;
+   };
 
   const getWeather = async() => {
     const wData = await useWeatherApi().getWeatherData(lat, lng)
@@ -17,6 +25,12 @@
       hour.currentTime = utc + 1000 * wData.data.timezone_offset;
     })
 
+    state.currentTime = wData.data.currentTime;
+
+    const currentTemperatureFahrenheit = wData.data.current.temp;
+    const currentTemperatureKelvin = convertToCelsius(currentTemperatureFahrenheit);
+    state.temp = currentTemperatureKelvin;
+
     return wData
   }
 
@@ -27,10 +41,39 @@
     } else {
       await getWeather()
     }
-    
+
   });
 </script>
 <template>
- 
+  <div class="flex flex-col flex-1 items-center">
+    <!-- Banner -->
+    <div 
+      v-if="route.query.preview"
+      class="text-white p-4 bg-weather-secondary w-full text-center">
+      <p>Vous êtes actuellement en train de prévisualiser cette ville, cliquez sur l'icône "+" pour commencer à suivre cette ville.</p>
+    </div>
+    <div class="flex flex-col items-center text-white py-12">
+      <h1 class="text-4xl mb-2"> {{ name.toUpperCase() }}</h1>
+      <p class="text-sm mb-12">
+        {{ 
+          new Date(state.currentTime).toLocaleDateString("fr-FR", 
+          { weekday: "short", // Jour de la semaine complet
+            year: "numeric", // Année au format numérique
+            month: "long", // Mois complet
+            day: "2-digit" // Jour du mois au format numérique
+          })
+        }}
+        {{ 
+          new Date(state.currentTime).toLocaleTimeString("fr-FR",
+          {
+            timeStyle: "short"
+          })
+        }}  
+      </p>
+      <p class="text-8xl mb-8">
+        {{ Math.round(state.temp) }}&deg;
+      </p>
+    </div>
+  </div>
 </template>
 
