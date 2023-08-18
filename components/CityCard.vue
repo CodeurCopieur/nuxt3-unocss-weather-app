5d6477909920e7acd12d7cafeb2fc672cd102940
Widdy LOUIS <louisws@outlook.fr>
2023-08-19 00:33:49 +0200

    Update CityCard.vue

--- components/CityCard.vue
+++ components/CityCard.vue
@@ -6,6 +6,7 @@
     },
     
   });
+  const { convertToCelsius, translateWeatherDescription } = useWeatherApi();
 
   const { city } = props;
 </script>
@@ -13,18 +14,18 @@
   <div class="flex py-6 px-3 bg-weather-secondary rounded-md shadow-md cursor-pointer">
     <div class="flex flex-col flex-1">
       <h2 class="text-3xl">{{ city.name }}</h2>
-      <h3>{{translateWeatherDescription(city.weather.weather[0].description) }}</h3>
+      <h3>{{ translateWeatherDescription(city.description) }}</h3>
     </div>
     <div class="flex flex-col gap-2">
       <p class="text-3xl self-end">
-        {{ Math.round(convertToCelsius(city.weather.main.temp)) }}&deg;
+        {{ Math.round(convertToCelsius(city.temp)) }}&deg;
       </p>
       <div class="flex gap-2">
         <span class="text-xs">
-          H: {{ Math.round(convertToCelsius(city.weather.main.temp_max)) }}&deg;
+          H: {{ Math.round(convertToCelsius(city.temp_max)) }}&deg;
         </span>
         <span class="text-xs">
-          B: {{ Math.round(convertToCelsius(city.weather.main.temp_min)) }}&deg;
+          B: {{ Math.round(convertToCelsius(city.temp_min)) }}&deg;
         </span>
       </div>
     </div>
