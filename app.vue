920a2513f7ccbcef21a85fe694332f614df40107
Widdy LOUIS <louisws@outlook.fr>
2023-08-22 23:03:58 +0200

    Route Transitions

--- app.vue
+++ app.vue
@@ -1,6 +1,27 @@
+<script setup>
+definePageMeta({
+  pageTransition: {
+    name: 'rotate'
+  }
+});
+</script>
+
+
 <template>
   <div class="flex flex-col min-h-screen bg-weather-primary">
     <SiteNavigation />
     <NuxtPage />
   </div>
 </template>
+
+<style>
+.page-enter-active,
+.page-leave-active {
+  transition: all 0.4s;
+}
+.page-enter-from,
+.page-leave-to {
+  opacity: 0;
+  filter: blur(1rem);
+}
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    pageTransition: { name: 'page', mode: 'out-in' }
+  },
   runtimeConfig: {
     public: {
       baseUrl: "https://api.mapbox.com/geocoding/v5/mapbox.places/",
