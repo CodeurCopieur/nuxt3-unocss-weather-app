42e4f4b9109252d5ede7faf46969c0fb2652f431
Widdy LOUIS <louisws@outlook.fr>
2023-08-22 19:47:07 +0200

    Update AsyncCityView.vue

--- components/AsyncCityView.vue
+++ components/AsyncCityView.vue
@@ -12,7 +12,8 @@
     description: null,
     icon: null,
     hourly: null,
-    daily: null
+    daily: null,
+    isLoading: true
   });
 
    const { getWeatherData, translateWeatherDescription, convertToCelsius } = useWeatherApi()
@@ -57,119 +58,127 @@
       useRouter().replace('/');
     } else {
       await getWeather()
+      setTimeout(() => {
+      state.isLoading = false;
+  }, 1000);
     }
 
   });
 </script>
 <template>
-  <div class="flex flex-col flex-1 items-center">
-    <!-- Banner -->
-    <div 
-      v-if="route.query.preview"
-      class="text-white p-4 bg-weather-secondary w-full text-center">
-      <p>Vous êtes actuellement en train de prévisualiser cette ville, cliquez sur l'icône "+" pour commencer à suivre cette ville.</p>
-    </div>
+  <template v-if="!state.isLoading">
+    <div class="flex flex-col flex-1 items-center">
+      <!-- Banner -->
+      <div 
+        v-if="route.query.preview"
+        class="text-white p-4 bg-weather-secondary w-full text-center">
+        <p>Vous êtes actuellement en train de prévisualiser cette ville, cliquez sur l'icône "+" pour commencer à suivre cette ville.</p>
+      </div>
 
-    <!-- Aperçu météo -->
-    <div class="flex flex-col items-center text-white py-12">
-      <h1 class="text-4xl mb-2"> {{ name.toUpperCase() }}</h1>
-      <p class="text-sm mb-12">
-        {{ 
-          new Date(state.currentTime).toLocaleDateString("fr-FR", 
-          { weekday: "short", // Jour de la semaine complet
-            year: "numeric", // Année au format numérique
-            month: "long", // Mois complet
-            day: "2-digit" // Jour du mois au format numérique
-          })
-        }}
-        {{ 
-          new Date(state.currentTime).toLocaleTimeString("fr-FR",
-          {
-            timeStyle: "short"
-          })
-        }}  
-      </p>
-      <p class="text-8xl mb-8">
-        {{ Math.round(state.temp) }}&deg;
-      </p>
-      <p>
-        Ressenti
-        {{ Math.round(state.feelsLike) }}&deg;
-      </p>
-      <p class="capitalize">
-        {{ translateWeatherDescription(state.description)  }}
-      </p>
-      <img 
-        class="w-[150px] h-auto"
-        :src="`http://openweathermap.org/img/wn/${state.icon}@2x.png`" alt="">
-    </div>
+      <!-- Aperçu météo -->
+      <div class="flex flex-col items-center text-white py-12">
+        <h1 class="text-4xl mb-2"> {{ name.toUpperCase() }}</h1>
+        <p class="text-sm mb-12">
+          {{ 
+            new Date(state.currentTime).toLocaleDateString("fr-FR", 
+            { weekday: "short", // Jour de la semaine complet
+              year: "numeric", // Année au format numérique
+              month: "long", // Mois complet
+              day: "2-digit" // Jour du mois au format numérique
+            })
+          }}
+          {{ 
+            new Date(state.currentTime).toLocaleTimeString("fr-FR",
+            {
+              timeStyle: "short"
+            })
+          }}  
+        </p>
+        <p class="text-8xl mb-8">
+          {{ Math.round(state.temp) }}&deg;
+        </p>
+        <p>
+          Ressenti
+          {{ Math.round(state.feelsLike) }}&deg;
+        </p>
+        <p class="capitalize">
+          {{ translateWeatherDescription(state.description)  }}
+        </p>
+        <img 
+          class="w-[150px] h-auto"
+          :src="`http://openweathermap.org/img/wn/${state.icon}@2x.png`" alt="">
+      </div>
 
-    <hr class="border-white border-opacity-10 border w-full" />
+      <hr class="border-white border-opacity-10 border w-full" />
 
-    <!-- Météo horaire -->
-    <div class="max-w-screen-md w-full py-12">
-      <div class="mx-8 text-white">
-        <h2 class="mb-4">Météo horaire</h2>
-        <div class="flex gap-10 overflow-x-scroll">
+      <!-- Météo horaire -->
+      <div class="max-w-screen-md w-full py-12">
+        <div class="mx-8 text-white">
+          <h2 class="mb-4">Météo horaire</h2>
+          <div class="flex gap-10 overflow-x-scroll">
+            <div 
+              class="flex flex-col gap-4 items-center"
+              v-for="data in state.hourly" :key="data.dt">
+              <p class="whitespace-nowrap text-md">
+                {{ 
+                  new Date(data.currentTime).toLocaleTimeString("fr-FR",
+                  {
+                    hour: "numeric"
+                  })
+                }}
+              </p>
+              <img 
+                class="h-[45px] w-auto object-cover"
+                :src="`http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`" alt="">
+              <p class="text-xl">
+                {{ Math.round(convertToCelsius(data.temp)) }}&deg;
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <hr class="border-white border-opacity-10 border w-full" />
+
+      <!-- Météo hebdomadaire -->
+      <div class="max-w-screen-md w-full py-12">
+        <div class="mx-8 text-white">
+          <h2 class="mb-4">Prévisions à 7 jours</h2>
           <div 
-            class="flex flex-col gap-4 items-center"
-            v-for="data in state.hourly" :key="data.dt">
-            <p class="whitespace-nowrap text-md">
+            class="flex items-center"
+            v-for="day in state.daily" 
+            :key="day.dt">
+            <p class="flex-1">
               {{ 
-                new Date(data.currentTime).toLocaleTimeString("fr-FR",
+                new Date(day.dt * 1000).toLocaleDateString("fr-FR",
                 {
-                  hour: "numeric"
+                  weekday: "long",
                 })
               }}
             </p>
             <img 
-              class="h-[45px] w-auto object-cover"
-              :src="`http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`" alt="">
-            <p class="text-xl">
-              {{ Math.round(convertToCelsius(data.temp)) }}&deg;
-            </p>
+                class="h-[50px] w-[50px] object-cover"
+                :src="`http://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png`" alt= />
+            <div class="flex gap-2 flex-1 justify-end">
+              <p>H: {{ Math.round(convertToCelsius(day.temp.max)) }}</p>
+              <p>B: {{ Math.round(convertToCelsius(day.temp.min)) }}</p>
+            </div>
           </div>
         </div>
       </div>
-    </div>
 
-    <hr class="border-white border-opacity-10 border w-full" />
-
-    <!-- Météo hebdomadaire -->
-    <div class="max-w-screen-md w-full py-12">
-      <div class="mx-8 text-white">
-        <h2 class="mb-4">Prévisions à 7 jours</h2>
-        <div 
-          class="flex items-center"
-          v-for="day in state.daily" 
-          :key="day.dt">
-          <p class="flex-1">
-            {{ 
-              new Date(day.dt * 1000).toLocaleDateString("fr-FR",
-              {
-                weekday: "long",
-              })
-            }}
-          </p>
-          <img 
-              class="h-[50px] w-[50px] object-cover"
-              :src="`http://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png`" alt= />
-          <div class="flex gap-2 flex-1 justify-end">
-            <p>H: {{ Math.round(convertToCelsius(day.temp.max)) }}</p>
-            <p>B: {{ Math.round(convertToCelsius(day.temp.min)) }}</p>
-          </div>
-        </div>
+      <div 
+        class="flex items-center gap-2 py-12 text-white cursor-pointer duration-150 hover:text-red-500"
+        @click="removeCity">
+        <Icon 
+          name="mdi-light:delete" 
+          class="text-xl hover:text-weather-secondary duration-150 cursor-pointer" />
+        <p>Supprimer la ville</p>
       </div>
     </div>
-
-    <div 
-      class="flex items-center gap-2 py-12 text-white cursor-pointer duration-150 hover:text-red-500"
-      @click="removeCity">
-      <Icon 
-        name="mdi-light:delete" 
-        class="text-xl hover:text-weather-secondary duration-150 cursor-pointer" />
-      <p>Supprimer la ville</p>
-    </div>
-  </div>
+  </template>
+  <template v-else>
+    <CityViewSkeleton />
+  </template>
 </template>
 
